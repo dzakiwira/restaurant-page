a139ea82d2971cd11fbc81743a201808563e1d1c
Dzaki Wiradinata <dzaki.wiradinata@gmail.com>
2022-08-29 11:23:51 -0400

    Imports function to build landing page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 import _ from 'lodash';
+import buildLanding from './landing';
 
-function component() {
-    const element = document.createElement('div');
-    const divid = document.getElementById('content');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    divid.innerHTML = 'restaurant';
-
-    return divid;
-  }
-  
-  document.body.appendChild(component());
+buildLanding();
