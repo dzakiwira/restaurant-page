c08128fcccedfc38b0e66723251b409609f87152
Dzaki Wiradinata <dzaki.wiradinata@gmail.com>
2022-08-30 13:35:59 -0400

    Added text and image for home page

---
+++ src/home.js
@@ -0,0 +1,33 @@
+function buildHome() {
+    const home = document.createElement('home');
+    home.classList.add('home');
+
+    const restText = document.createElement('div');
+    restText.classList.add('rest-text');
+
+    const restaurantDescription = document.createElement('h4');
+    restaurantDescription.classList.add('description');
+    restaurantDescription.textContent = 'Number one pizza in the world';
+
+    const restaurantPara = document.createElement('p');
+    restaurantPara.classList.add('para');
+    restaurantPara.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut turpis eros. Donec molestie sit amet mi in efficitur. Pellentesque sed scelerisque sapien. Curabitur enim augue, maximus non risus at, consequat efficitur ante. Maecenas tincidunt libero tempus, porta enim at, ornare lectus. Pellentesque vestibulum lobortis elit sed maximus.';
+
+    const orderNow = document.createElement('p');
+    orderNow.classList.add('order');
+    orderNow.textContent = 'Order Now!'
+
+    const pizzaImg = document.createElement('img');
+    pizzaImg.src = 'images/pizza.jpg';
+    pizzaImg.alt = 'Pizza';
+
+    restText.appendChild(restaurantDescription);
+    restText.appendChild(restaurantPara);
+    restText.appendChild(orderNow);
+    home.appendChild(restText);
+    home.appendChild(pizzaImg);
+
+    return home;
+}
+
+export default buildHome;
